69b19c762f8e33e8e0c9c47230d8a1288bf8fdd7
Maxime FAUBERTEAU <azramis.developer@gmail.com>
2023-06-23 21:19:51 +0200

    test: Comparison test

--- packages/valuable/test/valuable_test.dart
+++ packages/valuable/test/valuable_test.dart
@@ -1,19 +1,40 @@
 import 'package:valuable/valuable.dart';
 
-void main() {
+void main() async {
+  await Future(
+    () => print('Future #1'),
+  );
+  Future(
+    () => print('Future #1 bis'),
+  );
   Test().test();
+  await Future(
+    () => print('Future #2'),
+  );
 }
 
 class Test {
   final StatefulValuable<int> cpt = StatefulValuable<int>(0);
 
-  ValuableCallback get ecrire => ValuableCallback((ValuableWatcher watch,
-          {ValuableContext? valuableContext}) {
-        print(watch(cpt));
+  ValuableCallback get ecrire => ValuableCallback.immediate(
+          (ValuableWatcher watch, {ValuableContext? valuableContext}) {
+        print('Immediate : ${watch(cpt)}');
+      });
+
+  ValuableCallback get ecrireMicrotask => ValuableCallback.microtask(
+          (ValuableWatcher watch, {ValuableContext? valuableContext}) {
+        print('Microtask : ${watch(cpt)}');
+      });
+
+  ValuableCallback get ecrireFuture => ValuableCallback.future(
+          (ValuableWatcher watch, {ValuableContext? valuableContext}) {
+        print('Future : ${watch(cpt)}');
       });
 
   void test() {
     ecrire();
+    ecrireMicrotask();
+    ecrireFuture();
 
     for (int count = cpt.getValue(); count <= 10; count++) {
       cpt.setValue(count);
